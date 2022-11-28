6dc4640addeeea552aa7697f55143c7622109f15
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-11-28 13:39:36 -0600

    Arreglar funcion agregar comentario

--- models/comentario.php
+++ models/comentario.php
@@ -7,12 +7,17 @@ class Comentario extends Conexion {
     function insertarComentario($com, $idu_com, $ida_com) {
         $db = parent::connect();
         parent::set_names();
+        $sql = "SET FOREIGN_KEY_CHECKS = 0;";
+        $db->exec($sql);
         $sql = "INSERT INTO comentario(com, idu_com, ida_com) VALUES (?,?,?);";
         $sql = $db->prepare($sql);
         $sql->bindValue(1, $com);
-        $sql->bindValue(1, $idu_com);
-        $sql->bindValue(1, $ida_com);
-        return $sql->execute();
+        $sql->bindValue(2, $idu_com);
+        $sql->bindValue(3, $ida_com);
+        $result = $sql->execute();
+        $sql = "SET FOREIGN_KEY_CHECKS = 1;";
+        $db->exec($sql);
+        return $result;
     }
 
     function obtenerComentario($ida_com) {
